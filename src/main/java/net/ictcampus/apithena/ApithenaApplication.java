dd3c6c31da9e34632de18f0908b605cf54f390c0
Fabian Zaugg <fabian.zaugg@ict-campus.net>
2024-02-28 17:08:32 +0100

    new gitignore

---
+++ src/main/java/net/ictcampus/apithena/ApithenaApplication.java
@@ -0,0 +1,13 @@
+package net.ictcampus.apithena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApithenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApithenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ictcampus/apithena/ApithenaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ictcampus.apithena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApithenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
